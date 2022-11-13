fa29ded67f32e219ff69f68ff180f5eac608184d
Ahmad Alawi <ahmad.sami2009@hotmail.com>
2022-11-13 16:48:16 +0300

    Edit All

---
+++ components/Download.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="downloadSection" class="mt-16">
+    <h2 class="font-extrabold text-2xl">Download</h2>
+    <div class="stepC mt-6">
+      <p>Please complete the checklist to proceed,</p>
+      <Check
+        v-for="(item, index) in downloadCheckList"
+        :downloadCheckList="downloadCheckList"
+        :item="item"
+        :key="index"
+        :index="index"
+      />
+      <button
+        ref="downloadPackage"
+        @click="downloadPackage"
+        class="inline-block bg-gray-700 leading-none text-2xl tracking-wide border-2 border-transparent font-extrabold p-6 rounded mt-12 select-none transition-colors duration-200 focus:outline-none"
+        :title="
+          downloadChecked
+            ? ''
+            : 'Complete the checklist to download the package'
+        "
+        :class="
+          downloadChecked
+            ? 'bg-emerald-600 cursor-pointer text-white focus:bg-emerald-500 hover:bg-emerald-500'
+            : 'cursor-not-allowed text-black'
+        "
+      >
+        Download as ZIP
+      </button>
+      
+      
+    </div>
+  </div>
+</template>
+
+<script>
+import Check from '@/components/Check'
+export default {
+  props: ['downloadCheckList', 'downloadChecked', 'downloadPackage'],
+  components: {
+    Check,
+  },
+}
+</script>
