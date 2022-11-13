4ace63a7cc7e802d6dddf53fca9b240bc7860884
Ahmad Alawi <ahmad.sami2009@hotmail.com>
2022-11-13 16:52:24 +0300

    Edit All Files

---
+++ components/Footer.vue
@@ -0,0 +1,28 @@
+<template>
+  <footer class="mt-32 mx-4 flex flex-col">
+    <div class="flex flex-col justify-center items-center">
+      <div class="mt-6 border p-4 rounded border-gray-700 text-gray-400">
+
+        <div class="logo w-16 m-4"
+        
+        v-html="require(`~/assets/icons/logo.svg?include`)"
+        ></div>
+        <p >
+          Cashmere-Solutions
+        </p>
+        <p>
+          2022
+        </p>
+      </div>
+    </div>
+      
+    
+  </footer>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>
